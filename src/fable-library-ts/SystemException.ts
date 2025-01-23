4edc2a1c2cdf15b7a70bfee78aa45019e8240174
프리마 <fc1943s@pm.me>
2025-01-23 16:49:03 -0300

    tests

--- src/fable-library-ts/SystemException.ts
+++ src/fable-library-ts/SystemException.ts
@@ -1,4 +1,4 @@
-import { class_type, TypeInfo } from "./fable_modules/fable-library-ts.5.0.0-alpha.5/Reflection.js";
+import { class_type, TypeInfo } from "./Reflection.js";
 
 export class SystemException extends Error {
     constructor() {
